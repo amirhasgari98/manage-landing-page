7f6a98021698deef81408ca3ae047ca7691f37b0
amirhasgari <ah.asgari87@yahoo.com>
2022-10-05 16:20:37 +0330

    Finish commnets section with slider

--- scripts/app.js
+++ scripts/app.js
@@ -8,3 +8,24 @@ navToggleIcon.addEventListener("click", function () {
   menu.classList.toggle("menu--open");
   cover.classList.toggle("cover--show");
 });
+
+//Slider
+const swiper = new Swiper(".swiper", {
+  autoplay: {
+    delay: 3000,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 1,
+    },
+    992: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+  },
+});

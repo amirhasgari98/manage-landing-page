1c9890fb68b6f842e2ad44fe8b9f99814d44e4aa
amirhasgari <ah.asgari87@yahoo.com>
2022-10-11 19:21:42 +0330

    Finish Project

--- scripts/app.js
+++ scripts/app.js
@@ -2,9 +2,9 @@
 const navToggleIcon = document.querySelector(".nav__toggle-icon");
 const menu = document.querySelector(".menu");
 const cover = document.querySelector(".cover");
-let inputValue = document.querySelector(".input");
-let emailModal = document.querySelector(".footer-newsletter__modal");
-
+let inputValue = document.querySelector(".footer-newsletter__input");
+let emailModal = document.querySelector(".footer-newsletter__message");
+console.log(emailModal);
 navToggleIcon.addEventListener("click", function () {
   navToggleIcon.classList.toggle("nav__toggle-icon--open");
   menu.classList.toggle("menu--open");
@@ -34,13 +34,16 @@ const swiper = new Swiper(".swiper", {
 
 //Email verification
 
-inputValue.addEventListener("keydown", function (event) {
+inputValue.addEventListener("keyup", function (event) {
   emailModal.style.display = "block";
-  if (event.key === "@") {
+  if (inputValue.value.includes("@")) {
     emailModal.style.display = "none";
   }
   if (event.key === "Enter") {
     inputValue.value = "";
+    emailModal.style.display = "none";
+  }
+  if (inputValue.value === "") {
     emailModal.style.display = "none";
   }
 });
